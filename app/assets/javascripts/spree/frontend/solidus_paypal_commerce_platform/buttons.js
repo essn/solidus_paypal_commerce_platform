801adb952ca886ee7e052b14395b9e2b7df8f91f
Jesse Pledger <jessepledger@gmail.com>
2021-05-28 14:14:52 -0700

    Wrap render button functions with Spree.ready checks
    SolidusPaypalCommercePlatform/buttons.js functions will only excecute
    when Spree is ready avoiding the need for the developer to wrap the
    functions.

--- app/assets/javascripts/spree/frontend/solidus_paypal_commerce_platform/buttons.js
+++ app/assets/javascripts/spree/frontend/solidus_paypal_commerce_platform/buttons.js
@@ -1,29 +1,35 @@
 SolidusPaypalCommercePlatform.renderButton = function(payment_method_id, style) {
-  paypal.Buttons({
-    style: style,
-    createOrder: SolidusPaypalCommercePlatform.sendOrder.bind(null, payment_method_id),
-    onApprove: SolidusPaypalCommercePlatform.approveOrder,
-    onShippingChange: SolidusPaypalCommercePlatform.shippingChange,
-    onError: SolidusPaypalCommercePlatform.handleError
-  }).render('#paypal-button-container')
+  Spree.ready(() => {
+    paypal.Buttons({
+      style: style,
+      createOrder: SolidusPaypalCommercePlatform.sendOrder.bind(null, payment_method_id),
+      onApprove: SolidusPaypalCommercePlatform.approveOrder,
+      onShippingChange: SolidusPaypalCommercePlatform.shippingChange,
+      onError: SolidusPaypalCommercePlatform.handleError
+    }).render('#paypal-button-container')
+  })
 }
 
 SolidusPaypalCommercePlatform.renderCartButton = function(payment_method_id, style) {
-  paypal.Buttons({
-    style: style,
-    createOrder: SolidusPaypalCommercePlatform.sendOrder.bind(null, payment_method_id),
-    onApprove: SolidusPaypalCommercePlatform.finalizeOrder.bind(null, payment_method_id),
-    onShippingChange: SolidusPaypalCommercePlatform.shippingChange,
-    onError: SolidusPaypalCommercePlatform.handleError
-  }).render('#paypal-button-container')
+  Spree.ready(() => {
+    paypal.Buttons({
+      style: style,
+      createOrder: SolidusPaypalCommercePlatform.sendOrder.bind(null, payment_method_id),
+      onApprove: SolidusPaypalCommercePlatform.finalizeOrder.bind(null, payment_method_id),
+      onShippingChange: SolidusPaypalCommercePlatform.shippingChange,
+      onError: SolidusPaypalCommercePlatform.handleError
+    }).render('#paypal-button-container')
+  })
 }
 
 SolidusPaypalCommercePlatform.renderProductButton = function(payment_method_id, style) {
-  paypal.Buttons({
-    style: style,
-    createOrder: SolidusPaypalCommercePlatform.createAndSendOrder.bind(null, payment_method_id),
-    onApprove: SolidusPaypalCommercePlatform.finalizeOrder.bind(null, payment_method_id),
-    onShippingChange: SolidusPaypalCommercePlatform.shippingChange,
-    onError: SolidusPaypalCommercePlatform.handleError
-  }).render('#paypal-button-container')
+  Spree.ready(() => {
+    paypal.Buttons({
+      style: style,
+      createOrder: SolidusPaypalCommercePlatform.createAndSendOrder.bind(null, payment_method_id),
+      onApprove: SolidusPaypalCommercePlatform.finalizeOrder.bind(null, payment_method_id),
+      onShippingChange: SolidusPaypalCommercePlatform.shippingChange,
+      onError: SolidusPaypalCommercePlatform.handleError
+    }).render('#paypal-button-container')
+  })
 }
